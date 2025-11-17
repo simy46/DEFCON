325e0528eb5cf0114833dcfce2528b34296b9046
simy46 <missipsa.annane@polymtl.ca>
2025-11-16 21:08:11 -0500

    logger

--- src/utils/timer.py
+++ src/utils/timer.py
@@ -1,14 +1,18 @@
 import time
+import logging
+from config.consts import LOG_FILE_PREFIX
+
+logger = logging.getLogger(LOG_FILE_PREFIX)
 
 class Timer:
     def __init__(self, msg=""):
         self.msg = msg
     
     def __enter__(self):
-        print(f"\n[TIMER START] {self.msg}")
+        logger.info(f"\n[TIMER START] {self.msg}")
         self.start = time.time()
     
     def __exit__(self, exc_type, exc_value, traceback):
         end = time.time()
-        print(f"[TIMER END] {self.msg} ({end - self.start:.2f} sec)\n")
+        logger.info(f"[TIMER END] {self.msg} ({end - self.start:.2f} sec)\n")
         return False
